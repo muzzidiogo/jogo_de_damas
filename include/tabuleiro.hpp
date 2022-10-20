f21bae56a8612f4921941ed46dc5bd2836a5af6f
Teli300 <68508499+Teli300@users.noreply.github.com>
2022-10-20 18:45:06 -0300

    Update tabuleiro.hpp

--- include/tabuleiro.hpp
+++ include/tabuleiro.hpp
@@ -1,4 +1,19 @@
 #ifndef TABULEIRO_H
 #define TABULEIRO_H
 
+#include <vector>
+#include "peca.hpp"
+
+class Tabuleiro_t {
+private:
+    const static unsigned int TAMANHO;
+    std::vector<Peca_t> _tabuleiro;
+public:
+    Tabuleiro_t();
+    void RemoverPeca(Peca_t peca);
+    bool VerificarPosicao(Posicao_t posicao);
+    bool AtualizarTabuleiro(Posicao_t novaPosicao, Peca_t peca);
+    std::vector<Peca_t> get_tabuleiro();
+};
+
 #endif //TABULEIRO_H
