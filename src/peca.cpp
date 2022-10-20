95fdfe6a480b8fd0358784cab435129563b90752
Teli300 <68508499+Teli300@users.noreply.github.com>
2022-10-20 18:47:03 -0300

    Update peca.cpp

--- src/peca.cpp
+++ src/peca.cpp
@@ -1,1 +1,19 @@
 #include "../include/peca.hpp"
+
+Peca_t::Peca_t(bool dama, int cor, Posicao_t posicao){
+    _dama = dama;
+    _cor = cor;
+    _posicao = posicao;
+}
+
+void Peca_t::Capturar_Peca(Posicao_t posicao, Peca_t pecaCapturada) {
+
+}
+
+Posicao_t Peca_t::get_posicao() {
+    return _posicao;
+}
+
+int Peca_t::get_cor() {
+    return _cor;
+}
