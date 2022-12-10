f8ca87a30120bf9754d088ed0c5be7685a019861
SamuelFVG <samuelfelipeif@gmail.com>
2022-12-10 20:19:10 -0300

    Definição do código para engine

--- include/engine.hpp
+++ include/engine.hpp
@@ -1,13 +1,17 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include <cmath>
 #include <map>
 #include "peca.hpp"
 #include "tabuleiro.hpp"
 
 class Engine {
+    private:
+        Tabuleiro_t tabuleiro;
+        char cor;
+        std::map<Peca_t, int> resultados;
     public:
-        void rodaEngine(Tabuleiro_t tabuleiro);
+        void rodaEngine(Tabuleiro_t _tabuleiro, char _cor);
+    
 };
 #endif
