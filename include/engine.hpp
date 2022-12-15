637461cb2075e4ee8f95ffd1b9b5e9ac2b953b2d
SamuelFVG <samuelfelipeif@gmail.com>
2022-12-15 19:04:16 -0300

    Mudancas no codigo da engine

--- include/engine.hpp
+++ include/engine.hpp
@@ -3,22 +3,19 @@
 
 #include "peca.hpp"
 #include "tabuleiro.hpp"
-
-typedef struct {
-    Peca_t peca;
-    std::vector<Peca_t> pecasCapturar;
-    int numCapturas;
-} Informacoes_pecas_t;
+#include <vector>
 
 class Engine {
     private:
         int _numCapturas; 
-        std::vector<Peca_t> _pecasCapturar;
+        int _numCapturasMax;
+        std::vector<Posicao_t> _posRemover;
+        std::vector<Posicao_t> _posRemoverMax;
         bool conta_jogadas(Tabuleiro_t tabuleiro, Peca_t peca);
         void roda_engine(Tabuleiro_t tabuleiro, char cor);
         Tabuleiro_t * aloca_tabuleiro_com_captura(Tabuleiro_t tabuleiro, Movimento_t movimento, Peca_t peca);
     public:
-        Informacoes_pecas_t get_maior(Tabuleiro_t tabuleiro, char cor);
+        Dados_pecas_t get_maior(Tabuleiro_t tabuleiro, char cor);
 };
 
 #endif
--- src/engine.cpp
+++ src/engine.cpp
@@ -11,80 +11,84 @@ Tabuleiro_t * Engine::aloca_tabuleiro_com_captura(Tabuleiro_t tabuleiro, Movimen
 bool Engine::conta_jogadas(Tabuleiro_t tabuleiro, Peca_t peca) {
   if (tabuleiro.verifica_validade_captura(peca, {-1, -1})) { 
     Tabuleiro_t *tabuleiroNovo = aloca_tabuleiro_com_captura(tabuleiro, {-1, -1}, peca);
+    
     _numCapturas++; 
-
-    if (_numCapturas > quantidadeDeJogadasMax) {
-      quantidadeDeJogadasMax = _numCapturas;
-      pecasRemoverMax = pecasRemover;
-    }
+    _posRemover.push_back({peca.get_posicao().linha - 1, peca.get_posicao().coluna - 1});
+    
+    if (_numCapturas > _numCapturasMax) {
+      _numCapturasMax = _numCapturas;
+      _posRemoverMax.clear();
+      for (Posicao_t pos : _posRemover) {
+        _posRemoverMax.push_back(pos);
+      }
+    }    
     if(conta_jogadas(*tabuleiroNovo, peca)) return 1;
     delete tabuleiroNovo;    
   }
 
   if (tabuleiro.verifica_validade_captura(peca, {1, 1})) { 
-      Tabuleiro_t tabuleiroNovo = Tabuleiro_t();
-      tabuleiroNovo.copia_tabuleiro(tabuleiro);
-      tabuleiroNovo.captura_peca(peca, {linha + 1, coluna + 1}, {2, 2});
-      quantidadeDeJogadas++;
-
-      if (quantidadeDeJogadas > quantidadeDeJogadasMax) {
-        quantidadeDeJogadasMax = quantidadeDeJogadas;
-        pecasRemoverMax = pecasRemover;
+    Tabuleiro_t *tabuleiroNovo = aloca_tabuleiro_com_captura(tabuleiro, {1, 1}, peca);
+    
+    _numCapturas++; 
+    _posRemover.push_back({peca.get_posicao().linha + 1, peca.get_posicao().coluna + 1});
+    
+    if (_numCapturas > _numCapturasMax) {
+      _numCapturasMax = _numCapturas;
+      _posRemoverMax.clear();
+      for (Posicao_t pos : _posRemover) {
+        _posRemoverMax.push_back(pos);
       }
-      if(conta_jogadas(tabuleiroNovo, peca)) return 1;    
+    }    
+    if(conta_jogadas(*tabuleiroNovo, peca)) return 1;
+    delete tabuleiroNovo;    
   }
 
   if (tabuleiro.verifica_validade_captura(peca, {1, -1})){ 
-      Tabuleiro_t tabuleiroNovo = Tabuleiro_t();
-      tabuleiroNovo.copia_tabuleiro(tabuleiro);
-      tabuleiroNovo.captura_peca(peca, {linha + 1, coluna - 1}, {2, -2});
-      quantidadeDeJogadas++;
+    Tabuleiro_t *tabuleiroNovo = aloca_tabuleiro_com_captura(tabuleiro, {1, -1}, peca);
 
-      if (quantidadeDeJogadas > quantidadeDeJogadasMax) {
-        quantidadeDeJogadasMax = quantidadeDeJogadas;
-        pecasRemoverMax = pecasRemover;
+    _numCapturas++; 
+    _posRemover.push_back({peca.get_posicao().linha + 1, peca.get_posicao().coluna - 1});
+    
+    if (_numCapturas > _numCapturasMax) {
+      _numCapturasMax = _numCapturas;
+      _posRemoverMax.clear();
+      for (Posicao_t pos : _posRemover) {
+        _posRemoverMax.push_back(pos);
       }
-      if(conta_jogadas(tabuleiroNovo, peca)) return 1;    
+    }    
+    if(conta_jogadas(*tabuleiroNovo, peca)) return 1;
+    delete tabuleiroNovo;    
   }
   
   if (tabuleiro.verifica_validade_captura(peca, {-1, 1})) { 
-      Tabuleiro_t tabuleiroNovo = Tabuleiro_t();
-      tabuleiroNovo.copia_tabuleiro(tabuleiro);
-      tabuleiroNovo.captura_peca(peca, {linha - 1, coluna + 1}, {-2, 2});
-      quantidadeDeJogadas++;
-      
-      if (quantidadeDeJogadas > quantidadeDeJogadasMax) {
-        quantidadeDeJogadasMax = quantidadeDeJogadas;
-        pecasRemoverMax = pecasRemover;
+    Tabuleiro_t *tabuleiroNovo = aloca_tabuleiro_com_captura(tabuleiro, {-1, 1}, peca);
+    
+    _numCapturas++; 
+    _posRemover.push_back({peca.get_posicao().linha - 1, peca.get_posicao().coluna + 1});
+    
+    if (_numCapturas > _numCapturasMax) {
+      _numCapturasMax = _numCapturas;
+      _posRemoverMax.clear();
+      for (Posicao_t pos : _posRemover) {
+        _posRemoverMax.push_back(pos);
       }
-      if(conta_jogadas(tabuleiroNovo, peca)) return 1;            
+    }    
+    if(conta_jogadas(*tabuleiroNovo, peca)) return 1;
+    delete tabuleiroNovo;            
   }
 
-  quantidadeDeJogadas--;
+  _posRemover.pop_back();
+  _numCapturas--;
   return 0;    
 }
 
-void Engine::roda_engine(Tabuleiro_t tabuleiro, char cor) {
-  for (unsigned int i = 0; i < tabuleiro.get_tabuleiro().size(); i++) {
-    if (tabuleiro.get_tabuleiro()[i].get_cor() == cor) {
-      quantidadeDeJogadas = 0; quantidadeDeJogadasMax = 0;
-      pecasRemover.clear(); pecasRemoverMax.clear();
-      this->conta_jogadas(tabuleiro, tabuleiro.get_tabuleiro()[i]);
-      tabuleiro.get_tabuleiro()[i].set_quantidadeJogadas(quantidadeDeJogadasMax);
-      tabuleiro.get_tabuleiro()[i].set_pecasRemover(pecasRemoverMax);
+Peca_t Engine::get_maior(Tabuleiro_t tabuleiro, char cor) {
+  for (Peca_t peca : tabuleiro.get_tabuleiro()) {
+    if (peca.get_cor() == cor) {
+      _numCapturas = 0; _numCapturasMax = 0;
+      _posRemover.clear(); _posRemoverMax.clear();
+      this->conta_jogadas(tabuleiro, peca);
+      
     }
   }
-}
-
-Peca_t Engine::get_maior(Tabuleiro_t tabuleiro, char cor) {
-  this->roda_engine(tabuleiro, cor);
-
-  Peca_t maior = Peca_t(Cores::PRETO, {1, 1}); //definicao de peca inicial para comparacoes
-  maior.set_quantidadeJogadas(-1);
-  for (Peca_t peca : tabuleiro.get_tabuleiro()) {
-    if (peca.get_cor() == cor && 
-      peca.get_quantidadeJogadas() > maior.get_quantidadeJogadas()) maior = peca; 
-  }
-  
-  return maior;
 }
