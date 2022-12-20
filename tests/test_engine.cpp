017cce6eb8ae124472de98550d3f50be4e1249dc
SamuelFVG <samuelfelipeif@gmail.com>
2022-12-20 17:16:02 -0300

    Correcoes no codigo de testes

--- tests/test_engine.cpp
+++ tests/test_engine.cpp
@@ -13,7 +13,7 @@ TEST_CASE("Teste Tabuleiro Inicial") {
     delete pTest;
 }
 
-TEST_CASE("Teste Sem Captura 2") {
+TEST_CASE("Teste Sem Captura") {
     std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::PRETO,Posicao_t{1,1}), 
                                          Peca_t(Cores::BRANCO,Posicao_t{1,5})};
     Tabuleiro_t tabuleiro = Tabuleiro_t();
@@ -25,22 +25,22 @@ TEST_CASE("Teste Sem Captura 2") {
     CHECK_EQ(0, pTest->posRemover->size());
     delete pTest;
 }
-/*
+
 TEST_CASE("Teste Captura 1") {
-    std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{5,5}),
-                                         Peca_t(Cores::PRETO,Posicao_t{4,4}) };
+    std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{1,1}),
+                                         Peca_t(Cores::PRETO,Posicao_t{2,2}) };
     Tabuleiro_t tabuleiro = Tabuleiro_t();
     tabuleiro.set_tabuleiro_testes(NovoTabuleiro);
     Engine engine;
     PecaPorCapturas_t *pTest = engine.get_maior(tabuleiro, Cores::BRANCO);
     CHECK_EQ(tabuleiro.get_tabuleiro()[0], pTest->peca);
     CHECK_EQ(1, pTest->posRemover->size());
-    CHECK_EQ(4, pTest->posRemover->at(0).coluna);
-    CHECK_EQ(4, pTest->posRemover->at(0).linha);
+    CHECK_EQ(3, pTest->posRemover->at(0).coluna);
+    CHECK_EQ(3, pTest->posRemover->at(0).linha);
     delete pTest;
 }
 
-TEST_CASE("Teste Captura 2") {
+/*TEST_CASE("Teste Captura 2") {
     std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{5,5}),
                                          Peca_t(Cores::PRETO,Posicao_t{4,4}),
                                          Peca_t(Cores::PRETO,Posicao_t{2,4}) };
