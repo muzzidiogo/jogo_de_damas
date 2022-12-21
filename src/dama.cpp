47b02b05c238225cf548011de7854eeafaf46089
muzzidiogo <muzzidiogo@gmail.com>
2022-12-21 16:13:52 -0300

    Criando branch para implementar a dama

--- src/dama.cpp
+++ src/dama.cpp
@@ -1,7 +1,11 @@
 #include "../include/dama.hpp"
+#include <cmath>
 
 Dama::Dama(int cor, Posicao_t posicao): Peca_t(cor, posicao){};
 
 void Dama::andar(Posicao_t novaPosicao) {
-    
+    if(abs((int)get_posicao().linha - (int)novaPosicao.linha) == abs((int)get_posicao().coluna - (int)novaPosicao.linha)
+    && novaPosicao.linha < 8 && novaPosicao.coluna < 8){
+        andar(novaPosicao);
+    }
 }
