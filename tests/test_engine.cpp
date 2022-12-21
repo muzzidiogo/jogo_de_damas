478c4f6668f5cefd8e4c6c571d6f3c68e6984bb4
SamuelFVG <samuelfelipeif@gmail.com>
2022-12-21 15:56:49 -0300

    Testes engine

--- tests/test_engine.cpp
+++ tests/test_engine.cpp
@@ -3,7 +3,7 @@
 #include "../include/engine.hpp"
 #include "../include/tabuleiro.hpp"
 
-TEST_CASE("Teste sem captura 1") {
+TEST_CASE("Teste sem captura 1 (teste com tabuleiro padrao)") {
     Tabuleiro_t tabuleiro = Tabuleiro_t();
     Engine engine;
 
@@ -13,7 +13,7 @@ TEST_CASE("Teste sem captura 1") {
     CHECK_EQ(0, capturas.size());
 }
 
-TEST_CASE("Teste Sem Captura 2") {
+TEST_CASE("Teste Sem Captura 2 (teste com duas pecas somente)") {
     std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::PRETO,Posicao_t{1,1}), 
                                          Peca_t(Cores::BRANCO,Posicao_t{1,5})};
     Tabuleiro_t tabuleiro = Tabuleiro_t();
@@ -28,7 +28,7 @@ TEST_CASE("Teste Sem Captura 2") {
     CHECK_EQ(peca.get_posicao().coluna, NovoTabuleiro[1].get_posicao().coluna);
 } 
 
-TEST_CASE("Teste com captura 1") {
+TEST_CASE("Teste com captura 1 (teste inicial)") {
     std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::PRETO,Posicao_t{1,1}), 
                                          Peca_t(Cores::BRANCO,Posicao_t{2,2})};
     Tabuleiro_t tabuleiro = Tabuleiro_t();
@@ -45,7 +45,7 @@ TEST_CASE("Teste com captura 1") {
     CHECK_EQ(peca.get_posicao().coluna, NovoTabuleiro[1].get_posicao().coluna);
 }
 
-TEST_CASE("Teste sem captura 3") {
+TEST_CASE("Teste sem captura 3 (testes dos extremos)") {
     std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{0,0}), 
                                          Peca_t(Cores::PRETO,Posicao_t{1,1}),
                                          Peca_t(Cores::PRETO,Posicao_t{2,2}),
@@ -64,7 +64,7 @@ TEST_CASE("Teste sem captura 3") {
 }
 
 
-TEST_CASE("Teste sem captura 2") {
+TEST_CASE("Teste com captura 2 (teste de captura em todas as direcoes) (duas pecas podem capturar)") {
     std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{1,1}), 
                                          Peca_t(Cores::PRETO,Posicao_t{2,2}),
                                          Peca_t(Cores::PRETO,Posicao_t{4,4}),
@@ -88,6 +88,28 @@ TEST_CASE("Teste sem captura 2") {
     CHECK_EQ(capturas[2].coluna, NovoTabuleiro[5].get_posicao().coluna);
     CHECK_EQ(capturas[3].linha, NovoTabuleiro[6].get_posicao().linha);
     CHECK_EQ(capturas[3].coluna, NovoTabuleiro[6].get_posicao().coluna);
+    CHECK_EQ(peca.get_posicao().linha, NovoTabuleiro[0].get_posicao().linha);
+    CHECK_EQ(peca.get_posicao().coluna, NovoTabuleiro[0].get_posicao().coluna);
+}
+
+TEST_CASE("Teste com captura 3 (duas pecas podem capturar a mesma peca inimiga)") {
+    std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{1,1}), 
+                                         Peca_t(Cores::PRETO,Posicao_t{2,2}),
+                                         Peca_t(Cores::PRETO,Posicao_t{4,4}),
+                                         Peca_t(Cores::BRANCO, Posicao_t{3, 5}), //nao deve escolher esse caminho
+                                         };
+    Tabuleiro_t tabuleiro = Tabuleiro_t();
+    tabuleiro.set_tabuleiro_testes(NovoTabuleiro);
+    Engine engine;
+    Peca_t peca;
+    std::vector<Posicao_t> capturas;
+    engine.get_maior(tabuleiro, Cores::BRANCO, peca, capturas);
+
+    CHECK_EQ(capturas.size(), 2);
+    CHECK_EQ(capturas[0].linha, NovoTabuleiro[1].get_posicao().linha);
+    CHECK_EQ(capturas[0].coluna, NovoTabuleiro[1].get_posicao().coluna);
+    CHECK_EQ(capturas[1].linha, NovoTabuleiro[2].get_posicao().linha);
+    CHECK_EQ(capturas[1].coluna, NovoTabuleiro[2].get_posicao().coluna);
     CHECK_EQ(peca.get_posicao().linha, NovoTabuleiro[0].get_posicao().linha);
     CHECK_EQ(peca.get_posicao().coluna, NovoTabuleiro[0].get_posicao().coluna);
 }
