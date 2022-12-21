beececd7e4d30c9f182a731cb0771809396dabef
SamuelFVG <samuelfelipeif@gmail.com>
2022-12-21 15:52:46 -0300

    Testes engine

--- tests/test_engine.cpp
+++ tests/test_engine.cpp
@@ -45,11 +45,30 @@ TEST_CASE("Teste com captura 1") {
     CHECK_EQ(peca.get_posicao().coluna, NovoTabuleiro[1].get_posicao().coluna);
 }
 
-TEST_CASE("Teste com captura 2") {
+TEST_CASE("Teste sem captura 3") {
+    std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{0,0}), 
+                                         Peca_t(Cores::PRETO,Posicao_t{1,1}),
+                                         Peca_t(Cores::PRETO,Posicao_t{2,2}),
+                                         Peca_t(Cores::BRANCO, Posicao_t{6, 6}), //nao deve escolher esse caminho
+                                         Peca_t(Cores::PRETO, Posicao_t{5, 7}),
+                                         Peca_t(Cores::PRETO,Posicao_t{1,5}),
+                                         Peca_t(Cores::PRETO,Posicao_t{0,4})};
+    Tabuleiro_t tabuleiro = Tabuleiro_t();
+    tabuleiro.set_tabuleiro_testes(NovoTabuleiro);
+    Engine engine;
+    Peca_t peca;
+    std::vector<Posicao_t> capturas;
+    engine.get_maior(tabuleiro, Cores::BRANCO, peca, capturas);
+
+    CHECK_EQ(capturas.size(), 0);
+}
+
+
+TEST_CASE("Teste sem captura 2") {
     std::vector<Peca_t> NovoTabuleiro = {Peca_t(Cores::BRANCO,Posicao_t{1,1}), 
                                          Peca_t(Cores::PRETO,Posicao_t{2,2}),
                                          Peca_t(Cores::PRETO,Posicao_t{4,4}),
-                                         Peca_t(Cores::BRANCO, Posicao_t{7, 7}), //mao deve escolher esse caminho
+                                         Peca_t(Cores::BRANCO, Posicao_t{7, 7}), //nao deve escolher esse caminho
                                          Peca_t(Cores::PRETO, Posicao_t{6, 6}),
                                          Peca_t(Cores::PRETO,Posicao_t{4,6}),
                                          Peca_t(Cores::PRETO,Posicao_t{2,6})};
@@ -72,3 +91,4 @@ TEST_CASE("Teste com captura 2") {
     CHECK_EQ(peca.get_posicao().linha, NovoTabuleiro[0].get_posicao().linha);
     CHECK_EQ(peca.get_posicao().coluna, NovoTabuleiro[0].get_posicao().coluna);
 }
+
