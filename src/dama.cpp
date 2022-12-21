2c9875f73920b16467938e702e62a318b97b9b67
muzzidiogo <muzzidiogo@gmail.com>
2022-12-21 16:30:00 -0300

    verifica_movimento() na classe dama

--- src/dama.cpp
+++ src/dama.cpp
@@ -1,3 +1,11 @@
 #include "../include/dama.hpp"
 
 Dama::Dama(int cor, Posicao_t posicao): Peca_t(cor, posicao){};
+
+bool Dama::verifica_movimento(Posicao_t novaPosicao){
+    if(novaPosicao.linha < 8 && novaPosicao.coluna < 8
+    && abs((int)get_posicao().linha - (int)novaPosicao.linha)==abs((int)get_posicao().coluna - (int)novaPosicao.coluna)){
+        return true;
+    }
+    else return false;
+}
